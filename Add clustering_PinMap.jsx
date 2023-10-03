9f69a94b6727ff0ead3b3f7499d6286501315241
shivanandachakari <142482399+shivanandachakari@users.noreply.github.com>
2023-10-03 15:16:28 +0530

    Create Add clustering_PinMap.jsx

---
+++ Add clustering_PinMap.jsx
@@ -0,0 +1,172 @@
+/*global google*/
+
+import React, { Component, PropTypes } from "react";
+import ReactDOM from "react-dom";
+
+import { getSettingsForVisualization, setLatitudeAndLongitude, setCategory } from "metabase/lib/visualization_settings";
+import { hasLatitudeAndLongitudeColumns } from "metabase/lib/schema_metadata";
+
+import { LatitudeLongitudeError } from "metabase/visualizations/lib/errors";
+
+import _ from "underscore";
+import cx from "classnames";
+import L from "leaflet";
+import tinycolor from "tinycolor2";
+import ColorHash from "color-hash";
+const ch = new ColorHash();
+
+export default class PinMap extends Component {
+    static displayName = "Pin Map";
+    static identifier = "pin_map";
+    static iconName = "pinmap";
+
+    static isSensible(cols, rows) {
+        return hasLatitudeAndLongitudeColumns(cols);
+    }
+
+    static checkRenderable(cols, rows) {
+        if (!hasLatitudeAndLongitudeColumns(cols)) { throw new LatitudeLongitudeError(); }
+    }
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            lat: null,
+            lon: null,
+            zoom: null
+        };
+        _.bindAll(this, "onMapZoomChange", "onMapCenterChange", "updateSettings");
+    }
+
+    updateSettings() {
+        if (this.state.lat != null) {
+            this.props.onUpdateVisualizationSetting(["map", "center_latitude"], this.state.lat);
+        }
+        if (this.state.lon != null) {
+            this.props.onUpdateVisualizationSetting(["map", "center_longitude"], this.state.lon);
+        }
+        if (this.state.zoom != null) {
+            this.props.onUpdateVisualizationSetting(["map", "zoom"], this.state.zoom);
+        }
+        this.setState({ lat: null, lon: null, zoom: null });
+    }
+
+    onMapCenterChange(lat, lon) {
+        this.setState({ lat, lon });
+    }
+
+    onMapZoomChange(zoom) {
+        this.setState({ zoom });
+    }
+
+    averageCoordinate(coordinates) {
+        return _.reduce(coordinates, (memo, num) => {return memo + num}, 0) / coordinates.length;
+    }
+
+    genMarker(color){
+        return L.VectorMarkers.icon({
+            markerColor: color
+        });
+    }
+
+    genColorCode(string){
+        return tinycolor(ch.hex(string)).monochromatic()[3].toHexString();
+    }
+
+    componentDidMount() {
+        try {
+            let element = ReactDOM.findDOMNode(this.refs.map);
+
+            let { card, data } = this.props.series[0];
+
+            let settings = card.visualization_settings;
+            settings = getSettingsForVisualization(settings, "pin_map");
+            settings = setLatitudeAndLongitude(settings, data.cols);
+            settings = setCategory(settings, data.cols);
+
+            let latColIndex = settings.map.latitude_dataset_col_index;
+            let lonColIndex = settings.map.longitude_dataset_col_index;
+            let catColIndex = settings.map.category_dataset_col_index;
+
+            let center_latitude = settings.map.center_latitude;
+            let center_longitude = settings.map.center_longitude;
+            if (!center_latitude || !center_longitude){
+              center_latitude = this.averageCoordinate(_.pluck(data.rows, latColIndex));
+              center_longitude = this.averageCoordinate(_.pluck(data.rows, lonColIndex));
+            }
+
+
+            L.Icon.Default.imagePath = '/app/img';
+            let map = L.map(element).setView([center_latitude, center_longitude], settings.map.zoom);
+
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+              attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+              maxZoom: 18,
+            }).addTo(map);
+
+            let pruneCluster = new PruneClusterForLeaflet();
+            for (let row of data.rows) {
+              /* let options = {}
+               * if (catColIndex){
+               *   options.icon = this.genMarker(this.genColorCode(row[catColIndex]));
+               * }
+               * let marker = L.marker([row[latColIndex], row[lonColIndex]], options);
+
+               * let tooltipElement = document.createElement("div");
+               * ReactDOM.render(<ObjectDetailTooltip row={row} cols={data.cols} />, tooltipElement);
+               * marker.bindPopup(tooltipElement);*/
+
+              let tooltipElement = document.createElement("div");
+              ReactDOM.render(<ObjectDetailTooltip row={row} cols={data.cols} />, tooltipElement);
+
+              let marker = new PruneCluster.Marker(row[latColIndex], row[lonColIndex]);
+              marker.data.icon = this.genMarker(this.genColorCode(row[catColIndex]));
+              marker.data.popup = tooltipElement;
+              pruneCluster.RegisterMarker(marker);
+            }
+
+            map.addLayer(pruneCluster);
+
+            map.on('moveend', () => {
+              let center = map.getCenter();
+              this.onMapCenterChange(center.lat, center.lng);
+            });
+
+            map.on('zoomend', () => {
+              this.onMapZoomChange(map.getZoom());
+            })
+
+        } catch (err) {
+            console.error(err);
+            this.props.onRenderError(err.message || err);
+        }
+    }
+
+    render() {
+        const { className, isEditing } = this.props;
+        const { lat, lon, zoom } = this.state;
+        const disableUpdateButton = lat == null && lon == null && zoom == null;
+        return (
+            <div className={className + " PinMap relative"} onMouseDownCapture={(e) =>e.stopPropagation() /* prevent dragging */}>
+                <div className="absolute top left bottom right" ref="map"></div>
+                { isEditing ?
+                    <div className={cx("PinMapUpdateButton Button Button--small absolute top right m1", { "PinMapUpdateButton--disabled": disableUpdateButton })} onClick={this.updateSettings}>
+                        Save as default view
+                    </div>
+                : null }
+            </div>
+        );
+    }
+}
+
+const ObjectDetailTooltip = ({ row, cols }) =>
+    <table>
+        <tbody>
+            { cols.map((col, index) =>
+                <tr>
+                    <td>{col.display_name}</td>
+                    <td>{row[index]}</td>
+                </tr>
+            )}
+        </tbody>
+    </table>
